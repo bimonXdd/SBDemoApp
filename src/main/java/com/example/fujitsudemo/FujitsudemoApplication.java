902175317546b56a63990c592905a5fcff20edf8
bimonxxd <saimon.prii@gmail.com>
2024-03-10 15:33:31 +0200

    first commit

---
+++ src/main/java/com/example/fujitsudemo/FujitsudemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fujitsudemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujitsudemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FujitsudemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fujitsudemo/FujitsudemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fujitsudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FujitsudemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
